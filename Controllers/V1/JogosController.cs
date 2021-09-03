81501604beb82fd345de9f4044f945611c71d7df
Luciano Arruda <lucen4t@gmail.com>
2021-09-03 06:06:11 -0300

    Partes 1 e 2

---
+++ Controllers/V1/JogosController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogoDeJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+
+            return Ok();
+
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idJogo)
+        {
+
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<AcceptedResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
+
+
+
+    }
+}
+
---
+++ InputModel/JogoInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogoDeJogos.Controllers.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength =3, ErrorMessage ="O jogo precisa ter entre 3 e 100 caracteres em seu nome")]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength =1, ErrorMessage ="O nome da produtora deve ter entre 1 e 100 caracteres")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(1,100,)]
+    }
+}
